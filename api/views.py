f5cbd637f312fe70e6b2579573ecc0a83608ee11
Harikishan <harikishantk@gmail.com>
2022-07-01 13:21:23 +0530

    Refactored views and redirect to the template

--- api/views.py
+++ api/views.py
@@ -46,14 +46,10 @@ def GoogleCalendarRedirectView(request):
         client_secret=json_loaded['client_secret'],)
     service = build('calendar', 'v3', credentials=creds)
     now = datetime.datetime.utcnow().isoformat() + 'Z' # 'Z' indicates UTC time
-    print('Getting the upcoming 10 events')
     events_result = service.events().list(calendarId='primary', timeMin=now,
                                         maxResults=10, singleEvents=True,
                                         orderBy='startTime').execute()
     events = events_result.get('items', [])
-    print("Got the events result")
     if not events:
-        print("No upcoming events found.")
-    for event in events:
-        start = event['start'].get('dateTime', event['start'].get('date'))
-        print(start, event['summary'])
+        events.append('No events upcoming')
+    return render(request, "events_list.html", {'events':events})
